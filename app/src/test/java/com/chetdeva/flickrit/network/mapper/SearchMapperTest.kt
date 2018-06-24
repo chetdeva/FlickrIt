7711977612931e907fe7e3e75d3d7c2f712827ec
Chetan Sachdeva <chet.deva@gmail.com>
2018-06-24 22:21:11 +0530

    test(*): test FlickrUrlFactory

---
+++ app/src/test/java/com/chetdeva/flickrit/network/mapper/SearchMapperTest.kt
@@ -0,0 +1,8 @@
+package com.chetdeva.flickrit.network.mapper
+
+import org.junit.Assert.*
+
+/**
+ * @author chetansachdeva
+ */
+class SearchMapperTest
---
+++ app/src/test/java/com/chetdeva/flickrit/util/image/FlickrUrlFactoryTest.kt
@@ -0,0 +1,8 @@
+package com.chetdeva.flickrit.util.image
+
+import org.junit.Assert.*
+
+/**
+ * @author chetansachdeva
+ */
+class FlickrUrlFactoryTest
